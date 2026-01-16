e034fe7da6da82a1d3063b55218e6835c32ce315
jorgeplatero <jorge.platero@outlook.com>
2026-01-16 12:17:50 -0300

    feat: modularizando extensão limiter

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ import logging
 from flask import Flask, jsonify
 from sqlalchemy import inspect
 #extensões
-from api.extensions import db, jwt, swagger, bcrypt, cache, migrate
+from api.extensions import db, jwt, swagger, bcrypt, cache, migrate, limiter
 #configurações
 from api.config import Config, TestingConfig
 #blueprints
@@ -13,9 +13,6 @@ from api.routes.books import books_bp
 from api.routes.stats import stats_bp
 from api.routes.scrape import scrape_bp
 from api.routes.ml import ml_bp
-
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 from api.logs import register_access_log
 
@@ -57,6 +54,7 @@ def create_app(testing=False):
     swagger.init_app(app)
     bcrypt.init_app(app)
     cache.init_app(app)
+    limiter.init_app(app)
 
     #tratamento de erros do JWT
     register_jwt_handlers(jwt)
@@ -73,16 +71,9 @@ def create_app(testing=False):
     app.register_blueprint(scrape_bp, url_prefix='/api/v1/scrape')
     app.register_blueprint(ml_bp, url_prefix='/api/v1/ml')
 
-    limiter = Limiter(
-        key_func=get_remote_address,
-        app=app,
-        default_limits=["60 per minute"]
-    )
-
     #rota raiz
-    
     @app.route('/')
-    #@limiter.limit('1 per minute')
+    @limiter.limit('1 per minute')
     def home():
         return jsonify({
             'status': 'online',
--- api/extensions/__init__.py
+++ api/extensions/__init__.py
@@ -4,6 +4,8 @@ from flasgger import Swagger
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_caching import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 db = SQLAlchemy()
@@ -12,3 +14,7 @@ jwt = JWTManager()
 swagger = Swagger()
 bcrypt = Bcrypt()
 cache = Cache()
+limiter = Limiter(
+    key_func=get_remote_address, 
+    default_limits=['60 per minute']
+)
